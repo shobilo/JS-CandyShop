d3c548e5c69a56cbb5b93e1ab39cc2f0f4fe4b1f
Alex <58330458+shobilo@users.noreply.github.com>
2022-02-08 16:36:46 +0300

    API error handler middleware

---
+++ server/middleware/ErrorHandlingMiddleware.js
@@ -0,0 +1,13 @@
+const ApiError = require('../helpers/ApiError')
+
+module.exports = function (error, req, res, next) {
+    if (error instanceof ApiError) {
+        return res.status(err.status).json({
+            message: error.message
+        })
+    }
+
+    return res.status(500).json({
+        message: "Unexpected error"
+    })
+}

120305ff4d77a09d28316c9f473a546c8ed4fe17
sho_bilo <alex.shabanovich@bk.ru>
2023-06-08 17:57:51 +0300

    Update for deploy for fe for render

--- client/src/api/APIAccess.js
+++ client/src/api/APIAccess.js
@@ -1,11 +1,11 @@
 import axios from "axios"
 
 const $host = axios.create({
-  baseURL: "http://localhost:5000/api/"
+  baseURL: "https://candyshopnodeapp.onrender.com/api"
 })
 
 const $authHost = axios.create({
-  baseURL: "http://localhost:5000/api/"
+  baseURL: "https://candyshopnodeapp.onrender.com/api"
 })
 
 const authInterceptor = (config) => {

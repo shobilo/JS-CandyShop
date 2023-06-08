a3292fdd473ef4cfa377904801158f5ca4b1af6a
sho_bilo <alex.shabanovich@bk.ru>
2023-06-08 17:43:27 +0300

    Update for deploy for render

--- server/db.js
+++ server/db.js
@@ -1,13 +1,22 @@
 const {Sequelize} = require('sequelize')
 
+// module.exports = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//         dialect: 'postgres',
+//         host: process.env.DB_HOST,
+//         port: process.env.DB_PORT,
+//         logging: false,
+//     }
+// )
+
 module.exports = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        dialect: 'postgres',
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        logging: false,
+    "postgres://candyshopuser:iuDYM4U5hLifXfXPftdVwpVYLO7v8dQ6@dpg-chs8rhhmbg582k940hm0-a.frankfurt-postgres.render.com/candyshopname", {
+        dialect: "postgres",
+        dialectOptions: {
+            ssl: true
+        }
     }
 )
